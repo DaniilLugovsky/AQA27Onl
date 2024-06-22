078102cf9975c2b5a98e605f10648bf81f858220
DaniilLugivsky <propro.organav@gmail.com>
2024-06-22 17:18:31 +0300

    TestNG Homework

---
+++ src/test/java/data/Listener.java
@@ -0,0 +1,10 @@
+package data;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listener implements ITestListener {
+    public void onTestFailure(ITestResult result){
+        System.out.println("Make a screenshot!");
+    }
+}
