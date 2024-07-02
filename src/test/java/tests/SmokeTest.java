638d07453afbda2ab4c7336d51fd24fcfbd11653
DaniilLugivsky <propro.organav@gmail.com>
2024-07-02 20:08:02 +0300

    WebDriver

--- src/test/java/tests/SmokeTest.java
+++ src/test/java/tests/SmokeTest.java
@@ -4,15 +4,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import services.BrowsersService;
 
 public class SmokeTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new BrowsersService().getDriver();
+    }
+    @AfterMethod
+    public void setDown() {
+        driver.quit();
+    }
+
     @Test
     public void validateIKTCalculaion() throws InterruptedException {
 
-        WebDriver driver = new BrowsersService().getDriver();
 
         driver.get("https://clinic-cvetkov.ru/company/kalkulyator-imt/");
 
@@ -32,6 +46,34 @@ public class SmokeTest {
 
         Assert.assertEquals(result.getText(), "24.9 - Норма");
 
-        driver.quit();
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertEquals(result.getText(), "24.9 - Норма");
+        softAssert.assertEquals(result.getText(), "24.8 - Норма");
+        softAssert.assertEquals(button.getText(), "1Рассчитать");
+
+        softAssert.assertAll();
+
+    }
+
+
+    @Test
+    public void validateSKF() throws InterruptedException {
+        driver.get("https://bymed.top/calc/%D1%81%D0%BA%D1%84-2148");
+        Thread.sleep(2000);
+
+        driver.switchTo().frame(1);
+
+        WebElement selectWebElement = driver.findElement(By.id("cr-size"));
+        Select selectCreateninType = new Select(selectWebElement);
+
+        selectCreateninType.selectByIndex(1);
+        Thread.sleep(2000);
+
+        selectCreateninType.selectByValue("mg");
+        Thread.sleep(2000);
+
+        selectCreateninType.selectByVisibleText("мкмоль/л");
+        Thread.sleep(2000);
     }
 }
